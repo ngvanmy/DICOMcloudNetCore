26537f927dbd254a1a74b80024251e6b065f6a24
Nguyễn Văn Mỹ <ngvanmy114@gmail.com>
2022-12-27 10:21:13 +0700

    update

--- DICOMcloud.DataAccess.Database/MySQLDatabaseFactory.cs
+++ DICOMcloud.DataAccess.Database/MySQLDatabaseFactory.cs
@@ -1,43 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace DICOMcloud.DataAccess.Database
 {
-    using System.Data;
-    using MySql.Data;
-    using MySql.Data.MySqlClient;
-
-    namespace DICOMcloud.DataAccess.Database
+    public class MySQLDatabaseFactory : IDatabaseFactory
     {
-        public class MySQLDatabaseFactory : IDatabaseFactory
+        public MySQLDatabaseFactory( IConnectionStringProvider connectionStringProvider )
         {
-            public MySQLDatabaseFactory(IConnectionStringProvider connectionStringProvider)
-            {
-                ConnectionString = connectionStringProvider.ConnectionString;
-            }
+            ConnectionString = connectionStringProvider.ConnectionString ;
+        }
 
-            public MySQLDatabaseFactory(string connectionString)
-            {
-                ConnectionString = connectionString;
-            }
+        public MySQLDatabaseFactory( string connectionString )
+        {
+            ConnectionString = connectionString ;
+        }
 
-            public virtual IDbConnection CreateConnection()
-            {
-                return new MySqlConnection(ConnectionString);
-            }
+        public virtual IDbConnection CreateConnection ( )
+        {
+            return new MySqlConnection ( ConnectionString ) ;
+        }
+        
+        public string ConnectionString {  get; protected set ; }
 
-            public string ConnectionString { get; protected set; }
-
-            public virtual IDbCommand CreateCommand()
-            {
-                return new MySqlCommand();
-            }
-
-            public virtual IDbDataParameter CreateParameter(string parameterName, object value)
-            {
-                return new MySqlParameter(parameterName, value ?? System.DBNull.Value);
-            }
+        public virtual IDbCommand CreateCommand ( )
+        { 
+            return new MySqlCommand ( ) ;
+        }
+        
+        public virtual IDbDataParameter CreateParameter ( string parameterName, object value )
+        {
+            return new MySqlParameter (  parameterName, value?? System.DBNull.Value ) ;
         }
     }
 }
